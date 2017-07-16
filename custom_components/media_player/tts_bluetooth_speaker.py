4c3df01bf6b962616eb2a9c819115af9d3e854af
Petar Kozul <pkozul@yahoo.com>
2017-07-16 21:50:46 +1000

    Add options for pre and post silence in TTS

--- custom_components/media_player/tts_bluetooth_speaker.py
+++ custom_components/media_player/tts_bluetooth_speaker.py
@@ -28,16 +28,23 @@ import sys
 DEFAULT_NAME = 'TTS Bluetooth Speaker'
 DEFAULT_VOLUME = 0.5
 DEFAULT_CACHE_DIR = "tts"
+DEFAULT_SILENCE_DURATION = 0.0
 
 SUPPORT_BLU_SPEAKER = SUPPORT_PLAY_MEDIA | SUPPORT_VOLUME_SET
 
 CONF_ADDRESS = 'address'
 CONF_VOLUME = 'volume'
 CONF_CACHE_DIR = 'cache_dir'
+CONF_PRE_SILENCE_DURATION = 'pre_silence_duration'
+CONF_POST_SILENCE_DURATION = 'post_silence_duration'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_VOLUME, default=DEFAULT_VOLUME):
+        vol.All(vol.Coerce(float), vol.Range(min=0, max=1)),
+    vol.Optional(CONF_PRE_SILENCE_DURATION, default=DEFAULT_SILENCE_DURATION):
+        vol.All(vol.Coerce(float), vol.Range(min=0, max=1)),
+    vol.Optional(CONF_POST_SILENCE_DURATION, default=DEFAULT_SILENCE_DURATION):
         vol.All(vol.Coerce(float), vol.Range(min=0, max=1)),
     vol.Optional(CONF_CACHE_DIR, default=DEFAULT_CACHE_DIR): cv.string,
 })
@@ -49,9 +56,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
     address = config.get(CONF_ADDRESS)
     volume = float(config.get(CONF_VOLUME))
+    pre_silence_duration = float(config.get(CONF_PRE_SILENCE_DURATION))
+    post_silence_duration = float(config.get(CONF_POST_SILENCE_DURATION))
     cache_dir = get_tts_cache_dir(hass, config.get(CONF_CACHE_DIR))
 
-    add_devices([BluetoothSpeakerDevice(hass, name, address, volume, cache_dir)])
+    add_devices([BluetoothSpeakerDevice(hass, name, address, volume, pre_silence_duration, post_silence_duration, cache_dir)])
     return True
 
 def get_tts_cache_dir(hass, cache_dir):
@@ -63,7 +72,7 @@ def get_tts_cache_dir(hass, cache_dir):
 class BluetoothSpeakerDevice(MediaPlayerDevice):
     """Representation of a Bluetooth Speaker on the network."""
 
-    def __init__(self, hass, name, address, volume, cache_dir):
+    def __init__(self, hass, name, address, volume, pre_silence_duration, post_silence_duration, cache_dir):
         """Initialize the device."""
         self._hass = hass
         self._name = name
@@ -71,8 +80,11 @@ class BluetoothSpeakerDevice(MediaPlayerDevice):
         self._current = None
         self._address = address
         self._volume = volume
-        self._tracker = 'bluetooth_tracker' in sys.modules
+        self._pre_silence_duration = pre_silence_duration
+        self._post_silence_duration = post_silence_duration
         self._cache_dir = self.get_tts_cache_dir(cache_dir)
+        self._tracker = 'custom_components.device_tracker.bluetooth_tracker' in sys.modules
+        _LOGGER.debug('Bluetooth tracker integration:  {}'.format(str(self._tracker)))
 
     def get_tts_cache_dir(self, cache_dir):
         """Get cache folder."""
@@ -121,16 +133,29 @@ class BluetoothSpeakerDevice(MediaPlayerDevice):
         _LOGGER.info('play_media: %s', media_id)
         self._is_standby = False
 
+        media_file = self._cache_dir + '/' + media_id[media_id.rfind('/') + 1:];
+        sink = 'pulse::bluez_sink.' + re.sub(':', '_', self._address)
+        volume = str(self._volume * 100)
+
+        media_file_to_play = media_file
+
+        if (self._pre_silence_duration > 0) or (self._post_silence_duration > 0):
+            media_file_to_play = "/tmp/tts_{}".format(os.path.basename(media_file))
+            command = "sox {} {} pad {} {}".format(media_file, media_file_to_play, self._pre_silence_duration, self._post_silence_duration)
+            _LOGGER.debug('Executing command: %s', command)
+            subprocess.call(command, shell=True)
+
         if self._tracker:
             self._hass.services.call(bluetooth_tracker.DOMAIN, bluetooth_tracker.BLUETOOTH_TRACKER_SERVICE_TURN_OFF, None)
 
-        sink = 'pulse::bluez_sink.' + re.sub(':', '_', self._address)
-        volume = str(self._volume * 100)
-        media_file = self._cache_dir + '/' + media_id[media_id.rfind('/') + 1:];
-
-        command = "mplayer -ao {} -quiet -srate 44100 -channels 2 -volume {} {}".format(sink, volume, media_file);
+        command = "mplayer -ao {} -quiet -channels 2 -volume {} {}".format(sink, volume, media_file_to_play);
         _LOGGER.debug('Executing command: %s', command)
         subprocess.call(command, shell=True)
+
+        if (self._pre_silence_duration > 0) or (self._post_silence_duration > 0):
+            command = "rm {}".format(media_file_to_play);
+            _LOGGER.debug('Executing command: %s', command)
+            subprocess.call(command, shell=True)
 
         if self._tracker:
             self._hass.services.call(bluetooth_tracker.DOMAIN, bluetooth_tracker.BLUETOOTH_TRACKER_SERVICE_TURN_ON, None)
